0feefa57061bc4287dd79d63952e8bd4c327c2e7
Prince Williams <princewillzs@gmail.com>
2019-10-13 21:52:01 +0100

    Updates

---
+++ precache-manifest.8341f9e95b56522013346d64da20cf08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb75eebac2def219aa489499073c109b",
+    "url": "/trialblog/index.html"
+  },
+  {
+    "revision": "b6c802eaaee835a40499",
+    "url": "/trialblog/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "47efeeb29bd786823f10",
+    "url": "/trialblog/static/css/main.abbbd379.chunk.css"
+  },
+  {
+    "revision": "b6c802eaaee835a40499",
+    "url": "/trialblog/static/js/2.e4287314.chunk.js"
+  },
+  {
+    "revision": "47efeeb29bd786823f10",
+    "url": "/trialblog/static/js/main.09c6b0fd.chunk.js"
+  },
+  {
+    "revision": "c63171f11bb24b90de04",
+    "url": "/trialblog/static/js/runtime~main.2b25191e.js"
+  }
+]);
